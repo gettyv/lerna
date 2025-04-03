43d0391782860a5ab9f0dd4555a5803ef2cff5c4
Phreakester <gettyv05@gmail.com>
2025-04-03 01:34:52 -0400

    Test for 2 motors, encoders. update vel calc

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -22,7 +22,7 @@ void Encoder::updateVelocity(long currentPosition,
     long lastPosition, uint32_t currentUpdateTime, uint32_t lastUpdateTime) {
     long deltaPosition = currentPosition - lastPosition;
     // float deltaTime = ENCODER_VEL_UPDATE_PERIOD_US / 1.0e6;
-    float deltaTime = currentUpdateTime - lastUpdateTime;
+    float deltaTime = (currentUpdateTime - lastUpdateTime) / 1.0e6;
     float pulsesPerSec = deltaPosition / deltaTime;
     velocity = pulsesPerSec / ENCODER_PPR;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ uint32_t lastUpdateTime = NULL;
 uint32_t currentUpdateTime = NULL;
 
 Encoder encoderMotorA;
-// Encoder encoderMotorB;
+Encoder encoderMotorB;
 
 bool encoderALastStateA = NULL;
 bool encoderBLastStateA = NULL;
@@ -34,11 +34,11 @@ void updateEncoderVelocity() {
 
   encoderMotorA.updateVelocity(encoderAPosition, 
     encoderALastPosition, currentUpdateTime, lastUpdateTime);
-  // encoderMotorB.updateVelocity(encoderBPosition, 
-  //   encoderBLastPosition, currentUpdateTime, lastUpdateTime);
+  encoderMotorB.updateVelocity(encoderBPosition, 
+    encoderBLastPosition, currentUpdateTime, lastUpdateTime);
 
   encoderALastPosition = encoderAPosition;
-  // encoderBLastPosition = encoderBPosition;
+  encoderBLastPosition = encoderBPosition;
   lastUpdateTime = currentUpdateTime;
 
 }
@@ -57,15 +57,15 @@ void interruptUpdateEncoderA() {
   encoderALastStateA = currentStateA;
 }
 
-// void interruptUpdateEncoderB() {
-//   bool currentStateA = digitalRead(MOTORB_ENCODER_PIN_A);
-//   bool currentStateB = digitalRead(MOTORB_ENCODER_PIN_B);
+void interruptUpdateEncoderB() {
+  bool currentStateA = digitalRead(MOTORB_ENCODER_PIN_A);
+  bool currentStateB = digitalRead(MOTORB_ENCODER_PIN_B);
 
-//   encoderBPosition += encoderMotorB.encoderUpdateInterrupt(
-//     encoderBLastStateA, currentStateA, currentStateB);
+  encoderBPosition += encoderMotorB.encoderUpdateInterrupt(
+    encoderBLastStateA, currentStateA, currentStateB);
 
-//   encoderBLastStateA = currentStateA;
-// }
+  encoderBLastStateA = currentStateA;
+}
 
 // Where the magic happens!
 // void ctrlf() {
@@ -95,12 +95,12 @@ void setup() {
   pinMode(MOTORA_ENCODER_PIN_A, INPUT_PULLUP);
   pinMode(MOTORA_ENCODER_PIN_B, INPUT_PULLUP);
   encoderMotorA.begin();
-  // encoderMotorB.begin();
+  encoderMotorB.begin();
 
   encoderALastStateA = digitalRead(MOTORA_ENCODER_PIN_A);
-  // encoderBLastStateA = digitalRead(MOTORB_ENCODER_PIN_A);
+  encoderBLastStateA = digitalRead(MOTORB_ENCODER_PIN_A);
   attachInterrupt(digitalPinToInterrupt(MOTORA_ENCODER_PIN_A), interruptUpdateEncoderA, CHANGE);
-  // attachInterrupt(MOTORB_ENCODER_PIN_A, interruptUpdateEncoderB, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTORB_ENCODER_PIN_A), interruptUpdateEncoderB, CHANGE);
 
   // // Attach control function to timer
   // Serial.println("Attaching Timer");
@@ -118,19 +118,32 @@ void setup() {
   pinMode(MOTORB_PWM_PIN, OUTPUT);
   pinMode(MOTORA_CW_A_PIN, OUTPUT);
   pinMode(MOTORA_CCW_B_PIN, OUTPUT);
+  pinMode(MOTORB_CW_A_PIN, OUTPUT);
+  pinMode(MOTORB_CCW_B_PIN, OUTPUT);
 
   digitalWrite(MOTORA_CW_A_PIN, HIGH);
   digitalWrite(MOTORA_CCW_B_PIN, LOW);
   analogWrite(MOTORA_PWM_PIN, 0.2*255);
+
+  digitalWrite(MOTORB_CW_A_PIN, LOW);
+  digitalWrite(MOTORB_CCW_B_PIN, HIGH);
+  analogWrite(MOTORB_PWM_PIN, 0.2*255);
+  Serial.println("Setup Complete");
 }
 
 void loop() {
   // Nothing to see here :0  
   updateEncoderVelocity();
-  delay(1000);
+
   Serial.print("Encoder A Position: ");
   Serial.println(encoderAPosition);
   Serial.print("Encoder A Velocity: ");
   Serial.println(encoderMotorA.getVelocity());
+  Serial.print("Encoder B Position: ");
+  Serial.println(encoderBPosition);
+  Serial.print("Encoder B Velocity: ");
+  Serial.println(encoderMotorB.getVelocity());
+
+  delay(2000);
 
 }
